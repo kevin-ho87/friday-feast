456e475fbde3c5cf239225093dbec48fca9885df
kevin-ho87 <31581926+kevin-ho87@users.noreply.github.com>
2018-03-30 19:41:27 +1100

    Pages
    Pages setup, Router setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
 
 class App extends Component {
   constructor(props) {
@@ -8,8 +11,14 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Friday Feasts!</h1>
-        <p>Whose turn is it this week?!</p>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+        </nav>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
       </div>
     )
   }
