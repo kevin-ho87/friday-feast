14c0550fed3fc405c2f87cb9c5013c196cb575fb
kevin-ho87 <31581926+kevin-ho87@users.noreply.github.com>
2018-03-30 17:56:00 +1100

    App component
    Extract main App component

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Friday Feasts!</h1>
+        <p>Whose turn is it this week?!</p>
+      </div>
+    )
+  }
+}
+
+export default App
