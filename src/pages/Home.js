4c355ce3fae4320e34bc7c4e7d9605f3759950e1
kevin-ho87 <31581926+kevin-ho87@users.noreply.github.com>
2018-04-02 13:11:43 +1000

    Styled components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,23 @@
 // @flow
 import React, { Component } from 'react'
+import styled from 'styled-components'
 import DayOfWeek from '../components/DayOfWeek'
+
+const Holder = styled.div`
+  font-family: sans-serif;
+  background-color: #eee;
+  width: 400px;
+  padding: 1rem;
+  border-left: 4px solid #444;
+`
 
 class Home extends Component<void> {
   render() {
     return (
-      <div>
+      <Holder>
         <h1>Hi Megatron! It is your turn this week!</h1>
         <DayOfWeek dayIndex={5} />
-      </div>
+      </Holder>
     )
   }
 }
