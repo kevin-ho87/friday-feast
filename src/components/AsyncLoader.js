f229e247c4a1408cb177358dcfcc9c46489ddeb3
kevin-ho87 <31581926+kevin-ho87@users.noreply.github.com>
2018-04-02 13:20:30 +1000

    Flow Async loader component

--- src/components/AsyncLoader.js
+++ src/components/AsyncLoader.js
@@ -1,7 +1,8 @@
+// @flow
 import Loadable from 'react-loadable'
 import React from 'react'
 
-const AsyncLoader = func => {
+const AsyncLoader = (func: mixed) => {
   return Loadable({
     loader: func,
     loading: () => <p>Loading...</p>
