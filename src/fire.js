8551ed8fcd52a67ba951b2e4fbeac10772a4c996
kevin-ho87 <31581926+kevin-ho87@users.noreply.github.com>
2018-04-07 19:51:49 +1000

    Firebase
    adding firebase, setting up connection to users

---
+++ src/fire.js
@@ -0,0 +1,16 @@
+// @flow
+// Initialize Firebase
+import firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyAMTNg81_tsmDd2TAA9JwxpGQMWKVbPeWY",
+  authDomain: "friday-feasts.firebaseapp.com",
+  databaseURL: "https://friday-feasts.firebaseio.com",
+  projectId: "friday-feasts",
+  storageBucket: "friday-feasts.appspot.com",
+  messagingSenderId: "59209303252"
+}
+
+firebase.initializeApp(config)
+
+export default firebase
