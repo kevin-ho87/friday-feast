42f1d3cc79671f279f20e79838f0d90491801a05
kevin-ho87 <31581926+kevin-ho87@users.noreply.github.com>
2018-04-08 19:49:50 +1000

    Cloud functions
    http request to increment index. db listener to reset index

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,35 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+//exports.helloWorld = functions.https.onRequest((req, res) => {
+  //response.send("Hello from Firebase!");
+//});
+
+exports.activeIndex = functions.https.onRequest((req, res) => {
+  return admin.database().ref('/activeUserKey').transaction((theIndex) => {
+    return theIndex + 1;
+  }).then(() => {
+    return res.end();
+  })
+});
+
+
+exports.checkLength = functions.database.ref('/activeUserKey').onUpdate((change, context) => {
+
+  return admin.database().ref('/usersLength').once('value', function(snap){
+    const activeNumIndex = change.after.val() * 1;
+    const usersLengthNum = snap.val() * 1 - 1;
+    console.log('active user index is ', activeNumIndex);
+    console.log('length of users is', usersLengthNum);
+
+    if (activeNumIndex > usersLengthNum) {
+      console.log('change the value');
+      admin.database().ref('/activeUserKey').set(0);
+    }
+  });
+});
+
