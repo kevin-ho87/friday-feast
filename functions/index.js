ad25c234cc412f222787ed1a89b46bd81779ef7f
kevin-ho87 <31581926+kevin-ho87@users.noreply.github.com>
2018-04-09 21:01:13 +1000

    Cloud function users
    Set users length. On users update, if active user key is more than users length, set it to last

--- functions/index.js
+++ functions/index.js
@@ -22,11 +22,11 @@ exports.checkLength = functions.database.ref('/activeUserKey').onUpdate((change,
   return admin.database().ref('/usersLength').once('value', function(snap){
     const activeNumIndex = change.after.val() * 1;
     const usersLengthNum = snap.val() * 1 - 1;
-    console.log('active user index is ', activeNumIndex);
-    console.log('length of users is', usersLengthNum);
+    // console.log('active user index is ', activeNumIndex);
+    // console.log('length of users is', usersLengthNum);
 
     if (activeNumIndex > usersLengthNum) {
-      console.log('change the value');
+      // console.log('change the value');
       admin.database().ref('/activeUserKey').set(0);
     }
   });
@@ -34,5 +34,16 @@ exports.checkLength = functions.database.ref('/activeUserKey').onUpdate((change,
 
 exports.updateLength = functions.database.ref('/users').onWrite((change, context) => {
   const usersLength = change.after.val().length;
-  return admin.database().ref('/usersLength').set(usersLength);
+  // Set users length on users array updated
+  admin.database().ref('/usersLength').set(usersLength);
+
+  return admin.database().ref('/activeUserKey').once('value', function(snap){
+    const activeIndexVal = snap.val() * 1;
+    const lastItem = usersLength * 1 -1;
+
+    if (activeIndexVal > lastItem) {
+      // Update active user index to last if index is more than total users when users is updated
+      admin.database().ref('/activeUserKey').set(lastItem);
+    }
+  });
 });
