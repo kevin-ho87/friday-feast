365a5e41bbfaf4ade41824abd1fd4fdd16d05a3b
kevin-ho87 <31581926+kevin-ho87@users.noreply.github.com>
2018-04-16 20:38:44 +1000

    Home
    Retrieve and display active user/person

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
 // @flow
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import styled from 'styled-components'
 import DayOfWeek from '../components/DayOfWeek'
+import firebase from '../fire'
 
 const Holder = styled.div`
   background-color: #fff;
@@ -10,12 +11,50 @@ const Holder = styled.div`
   border-left: 4px solid #444;
 `
 
-class Home extends Component<void> {
+type State = {
+  isLoaded: boolean,
+  activeUserName: string
+}
+
+class Home extends Component<{}, State> {
+  constructor() {
+    super()
+
+    this.state = {
+      isLoaded: false,
+      activeUserName: ''
+    }
+  }
+
+  componentDidMount() {
+    //Firebase in here
+    const itemsRef = firebase.database().ref('users').once('value')
+    const activeUserIndex = firebase.database().ref('activeUserKey').once('value')
+
+    let promises = [itemsRef, activeUserIndex]
+
+    Promise.all(promises).then(values => {
+      const users = values[0].val()
+      const activeUserIndex: number = values[1].val()
+
+      this.setState({
+        isLoaded: true,
+        activeUserName: users[activeUserIndex].name
+      })
+    })
+  }
+
   render() {
     return (
       <Holder>
-        <h1>Hi Megatron! It is your turn this week!</h1>
-        <DayOfWeek dayIndex={5} />
+        { !this.state.isLoaded ?
+          <p>Rendering...</p>
+        :
+          <Fragment>
+            <h1>Hi {this.state.activeUserName}! It is your turn this week!</h1>
+            <DayOfWeek dayIndex={5} />
+          </Fragment>
+        }
       </Holder>
     )
   }
