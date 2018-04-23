82fa41d06487131672479988e2a8008e0899e4a0
kevin-ho87 <31581926+kevin-ho87@users.noreply.github.com>
2018-04-23 20:35:21 +1000

    Firebase extract
    import required firebase packages only

--- src/fire.js
+++ src/fire.js
@@ -1,6 +1,8 @@
 // @flow
 // Initialize Firebase
-import firebase from 'firebase'
+import { firebase } from '@firebase/app'
+import '@firebase/database'
+import '@firebase/auth'
 
 const config = {
   apiKey: process.env.APP_APIKEY,
