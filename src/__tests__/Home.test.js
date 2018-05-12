2c8a7c43372bf742989ff9352eaed166596cc3b0
kevin-ho87 <31581926+kevin-ho87@users.noreply.github.com>
2018-05-12 19:30:10 +1000

    Home Tests
    update home snapshot test

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -4,30 +4,21 @@ import toJson from 'enzyme-to-json'
 
 import Home from '../pages/Home'
 
-jest.mock('../fire', () => {
-
-  const firebasemock = require('firebase-mock');
-
-  const mockdatabase = new firebasemock.MockFirebase();
-  const mockauth = new firebasemock.MockFirebase();
-  const mocksdk = new firebasemock.MockFirebaseSdk(path => {
-    return path ? mockdatabase.child(path) : mockdatabase;
-  }, () => {
-    return mockauth;
+describe('Should render Home page/component', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(<Home />);
   });
 
-  const firebase = mocksdk.initializeApp(); // can take a path arg to database url
-  // optional - expose the mock
-  global.firebase = firebase;
-
-  // return the mock to match your export api
-  return firebase;
-});
-
-describe('Should render Home page/component', () => {
-
   it('snapshot should work', () => {
-    const wrapper = mount(<Home />)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  it('edit state data', () => {
+    wrapper.setState({
+      isLoaded: true,
+      activeUserName: 'Megatron'
+    });
     expect(toJson(wrapper)).toMatchSnapshot()
   })
 
--- src/__tests__/setupTests.js
+++ src/__tests__/setupTests.js
@@ -2,3 +2,24 @@ import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 configure({ adapter: new Adapter() })
+
+jest.mock('../fire', () => {
+
+  const firebasemock = require('firebase-mock');
+
+  const mockdatabase = new firebasemock.MockFirebase();
+  const mockauth = new firebasemock.MockFirebase();
+  const mocksdk = new firebasemock.MockFirebaseSdk(path => {
+    return path ? mockdatabase.child(path) : mockdatabase;
+  }, () => {
+    return mockauth;
+  });
+
+  const firebase = mocksdk.initializeApp(); // can take a path arg to database url
+
+  // optional - expose the mock
+  global.firebase = firebase;
+
+  // return the mock to match your export api
+  return firebase;
+});
