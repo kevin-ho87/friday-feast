64b66cd94c1ebf6ba9d0aee5ce96f199173aefa9
kevin-ho87 <31581926+kevin-ho87@users.noreply.github.com>
2018-05-27 16:08:53 +1000

    UserItem tests

--- src/__tests__/UserItem.test.js
+++ src/__tests__/UserItem.test.js
@@ -17,14 +17,6 @@ describe('UserItem component', () => {
     expect(toJson(wrapper)).toMatchSnapshot()
   })
 
-  it('Component active state', () => {
-    const wrapper = shallow(<UserItem
-      isActive="true"
-    />)
-
-    expect(wrapper.props().isActive).toEqual(true)
-  })
-
   it('Displays usertext at start', () => {
     const wrapper = shallow(<UserItem
       name="Megatron"
@@ -32,18 +24,79 @@ describe('UserItem component', () => {
 
     expect(wrapper.find('UserItem__ListBox')).toHaveLength(1)
   })
+})
 
+describe('UserItem component edit actions', () => {
   it('Displays edit form when in edit mode', () => {
-    const wrapper = shallow(<UserItem
-      name="Megatron"
-    />)
+    const wrapper = shallow(<UserItem name="Megatron" />)
 
     wrapper.find('UserItem__ListBox').find('Button').at(0).simulate('click')
     expect(wrapper.find('UserItem__EditForm')).toHaveLength(1)
   })
 
+  it('Input change updates state', () => {
+    const wrapper = mount(<UserItem name="gg" />)
 
+    wrapper.setState({ isEdit: true })
 
+    const theInput = wrapper.find('input')
+    theInput.instance().value = 'totoro'
+    theInput.simulate('change')
+
+    expect(wrapper.state('name')).toEqual('totoro')
+  })
+
+  it('New name prop data should overwrite name state', () => {
+    const wrapper = shallow(<UserItem name="Megatron" />)
+
+    wrapper.setState({ name: 'Zoro' })
+    wrapper.setProps({ name: 'Chopper' })
+
+    expect(wrapper.state('name')).toEqual('Chopper')
+  })
+})
+
+describe('UserItem component setActive actions', () => {
+  it('Component active state', () => {
+    const wrapper = shallow(<UserItem isActive="true" />)
+
+    expect(wrapper.props().isActive).toEqual(true)
+  })
+
+  it('onActiveUser fn prop did call on active set', () => {
+    const mockActive = jest.fn()
+    const wrapper = shallow(<UserItem index={2} onActiveUser={mockActive} />)
+
+    wrapper.find('UserItem__ListBox').find('Button').at(2).simulate('click')
+
+    expect(mockActive).toBeCalledWith(2)
+  })
+
+  it('Component when active, set active button is disabled', () => {
+    const wrapper = shallow(<UserItem isActive="true" />)
+    const activeBtn = wrapper.find('UserItem__ListBox').find('Button').at(2)
+    expect(activeBtn.props().disabled).toEqual(true)
+  })
 
 })
 
+describe('UserItem component delete actions', () => {
+  it('Window Modal confirm calls', () => {
+    window.confirm = jest.fn()
+    const wrapper = shallow(<UserItem />)
+
+    wrapper.find('UserItem__ListBox').find('Button').at(1).simulate('click')
+
+    expect(window.confirm).toBeCalled()
+  })
+
+  it('onRemove fn prop did call with uid', () => {
+    const mockRemove = jest.fn()
+    window.confirm = jest.fn(() => true) // Mock modal clicked ok
+    const wrapper = shallow(<UserItem uid={3} onRemove={mockRemove} />)
+
+    wrapper.find('UserItem__ListBox').find('Button').at(1).simulate('click')
+
+    expect(mockRemove).toBeCalledWith(3)
+  })
+})
